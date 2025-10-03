b95c7cf9308d07e9371c596052a0d6660911ab68
DuyVo2005 <vlkd1001@gmail.com>
2025-10-03 09:42:58 +0700

    up bai 1 spring security

---
+++ SpringSecurity/src/main/java/vn/khanhduy/configs/SecurityConfig.java
@@ -0,0 +1,56 @@
+package vn.khanhduy.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	// authentication
+	public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+		UserDetails admin = User.withUsername("duy").password(encoder.encode("12345")).roles("ADMIN").build();
+
+		UserDetails user = User.withUsername("user").password(encoder.encode("12345")).roles("USER").build();
+
+		return new InMemoryUserDetailsManager(admin, user);
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		return http.csrf(csrf -> csrf.disable())
+				.authorizeHttpRequests(
+						auth -> auth.requestMatchers("/").permitAll().requestMatchers("/customer/**").hasRole("ADMIN")
+								.anyRequest().authenticated())
+				.formLogin(login -> login.successHandler((request, response, authentication) -> {
+					System.out.println(">>> Logged in as: " + authentication.getName());
+					System.out.println(">>> Roles: " + authentication.getAuthorities());
+					response.sendRedirect("/");
+				})).build();
+	}
+
+	/*
+	 * @Bean public SecurityFilterChain securityFilterChain(HttpSecurity http)
+	 * throws Exception { return http.csrf(csrf ->
+	 * csrf.disable()).authorizeHttpRequests( auth ->
+	 * auth.requestMatchers("/").permitAll().requestMatchers("/customer/**").
+	 * authenticated() // .anyRequest().authenticated()
+	 * ).formLogin(Customizer.withDefaults()).build(); }
+	 */
+}
